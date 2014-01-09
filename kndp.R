597b90da6f0e0a19a040f9110631381140ae52fb
Goldbio <cwkeum@gmail.com>
2014-01-09 10:07:02 +0000

    kndp analysis

--- kndp.R
+++ kndp.R
@@ -25,9 +25,15 @@ data_after_regi=merge_data[ date >= regi_date ]
 
 
 # Incidence in Person-Year
-ni=data_after_regi[!duplicated(JID) & first_date < as.Date('2011-01-01'), length(JID) ]  # Number of incidence
+ni=data_after_regi[!duplicated(JID) & regi_ste<=first_date  &first_date < as.Date('2011-01-01'), length(JID) ]  # Number of incidence
 pd=data_after_regi[!duplicated(JID)& regi_date<=first_date , sum( first_date-regi_date) ] # Person-day 
 incidence = (ni/pd)*365 # Incidence in Person-Year   
+
+
+## Personal cost according to complications
+cost=data_after_regi[ ,tot.cost=sum(EDEC_TRAMT)  , by=list(JID, pat=first_date<=date ) ] 
+t.test( tot.cost~pat, data= cost )
+
 
 
 # Find significant variables 
